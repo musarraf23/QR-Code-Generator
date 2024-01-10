2e983c40e782ebf6aa64a919b157076c1685c7ac
musarraf23 <musarrafmansoori1025@gmail.com>
2024-01-10 20:09:02 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let imgBox = document.getElementById("imgBox");
+let qrImg = document.getElementById("qrImg");
+let qrText = document.getElementById("qrText");
+function GenerateQR()
+{
+    qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    imgBox.classList.add("show-img");
+}
